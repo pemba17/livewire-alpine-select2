5d47be6c2d7b900c8731d40879cc162278aa4792
pemba17 <pemba.nuru59@gmail.com>
2023-03-11 21:02:33 +0545

    comment added

--- resources/views/livewire/test-select.blade.php
+++ resources/views/livewire/test-select.blade.php
@@ -1,3 +1,4 @@
+{{-- Just Load jquery and select 2 from CDN and load in <head> --}}
 @push('select2')
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
